b90bfd2a2086fab3a956c247c1ab29e5960f36b3
Ubaid Badar <m.ubaidbadar@gmail.com>
2019-10-27 22:11:11 +0500

    made_design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import sound1 from './sounds/bubbles.mp3';
+import sound2 from './sounds/clay.mp3'
+import sound3 from './sounds/confetti.mp3';
+import sound4 from './sounds/glimmer.mp3';
+import sound5 from './sounds/moon.mp3';
+import sound6 from './sounds/ufo.mp3';
 
 function App() {
+  const colors = ['#ef5350', '#7e57c2', '#26a69a', '#8bc34a', '#ff7043', '#9c27b0'];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tap Music</h1>
+      <p>Make music with only one tap</p>
+      <div className='Visuals'></div>
+
+      <div className='music'>
+        {colors.map((color, index) => (
+          <div style={{ backgroundColor: color }}></div>
+        ))}
+      </div>
     </div>
   );
 }
