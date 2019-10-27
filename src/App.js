bd32536ca923cd26e767d6c7814d1cab5a67d43e
Ubaid Badar <m.ubaidbadar@gmail.com>
2019-10-27 22:40:02 +0500

    add_visuals_and_removed_double_click_issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './App.css';
 import sound1 from './sounds/bubbles.mp3';
 import sound2 from './sounds/clay.mp3'
@@ -8,21 +8,35 @@ import sound5 from './sounds/moon.mp3';
 import sound6 from './sounds/ufo.mp3';
 
 function App() {
+  const visual = useRef();
   const colors = ['#ef5350', '#7e57c2', '#26a69a', '#8bc34a', '#ff7043', '#9c27b0'];
   const sounds = [sound1, sound2, sound3, sound4, sound5, sound6]
-  const runMusic = (elm) => {
-    elm.target.childNodes[0].play();
+
+  const runMusic = (e, i) => {
+    const music = e.target.childNodes[0];
+    music.currentTime = 0;
+    music.play();
+
+    let ball = document.createElement('div');
+    visual.current.appendChild(ball);
+    ball.style.background = colors[i];
+    ball.addEventListener('animationend', (event) => {
+      visual.current.removeChild(event.target);
+    })
   }
   return (
     <div className="App">
-      <h1>Tap Music</h1>
-      <p>Make music with only one tap</p>
-      <div className='Visuals'></div>
+      <div>
+        <h1>Tap Music</h1>
+        <p>Make music with only one tap</p>
+      </div>
+
+      <div className='Visuals' ref={visual} ></div>
 
       <div className='music'>
-        {colors.map((color, index) => (
-          <div style={{ backgroundColor: color }} onClick={runMusic} key={color}>
-            <audio src={sounds[index]} />
+        {colors.map((color, i) => (
+          <div style={{ backgroundColor: color }} onClick={(e) => { runMusic(e, i) }} key={color}>
+            <audio src={sounds[i]} />
           </div>
         ))}
       </div>
